564bf612a65f79904a1843b2b6798cd275a968f5
Dussadee Wang <duss@Dussadees-MBP.attlocal.net>
2018-09-30 19:02:02 -0700

    add additional tests for dispatch for HomePage component index

--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -5,6 +5,8 @@ import { shallow } from 'enzyme';
 import HomePageList from 'containers/HomePageList';
 import ErrorPage from 'components/ErrorPage';
 import Header from 'components/Header';
+import { saveQuote } from 'containers/App/actions';
+import { inputQuote } from '../actions';
 import { HomePage, mapDispatchToProps } from '../index';
 import messages from '../messages';
 
@@ -43,12 +45,37 @@ describe('<HomePage />', () => {
         const result = mapDispatchToProps(dispatch);
         expect(result.onInputChange).toBeDefined();
       });
+      it('should dispatch inputQuote when called', () => {
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+        const quote = 'shoot for the moon and the stars';
+        result.onInputChange({ target: { value: quote } });
+        expect(dispatch).toHaveBeenCalledWith(inputQuote(quote));
+      });
 
-      describe('onSubmitForm', () => {
+      describe('handleFormSubmit', () => {
         it('should be mapped to Component props', () => {
           const dispatch = jest.fn();
           const result = mapDispatchToProps(dispatch);
           expect(result.handleFormSubmit).toBeDefined();
+        });
+        it('should dispatch saveQuote when called', () => {
+          const dispatch = jest.fn();
+          const preventDefault = jest.fn();
+          const target = { reset: () => {} };
+          const result = mapDispatchToProps(dispatch);
+          const event = { preventDefault, target };
+          result.handleFormSubmit(event);
+          expect(dispatch).toHaveBeenCalledWith(saveQuote());
+        });
+
+        it('should preventDefault if called with event', () => {
+          const preventDefault = jest.fn();
+          const target = { reset: () => {} };
+          const result = mapDispatchToProps(() => {});
+          const event = { preventDefault, target };
+          result.handleFormSubmit(event);
+          expect(preventDefault).toHaveBeenCalledWith();
         });
       });
     });
